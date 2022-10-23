dd7db353b3de8228940b8d76881976b5448f6844
h20913 <h20913@gmail.com>
2022-10-23 23:50:14 +0900

    ui update

--- Friday.MobileApplication/Platforms/Android/MainActivity.cs
+++ Friday.MobileApplication/Platforms/Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
+using AndroidX.Core.View;
 
 namespace Friday.MobileApplication;
 
@@ -15,4 +17,35 @@ namespace Friday.MobileApplication;
     DataMimeType = "*/*")]
 public class MainActivity : MauiAppCompatActivity
 {
+    private static MainActivity MainActivityInstance;
+    public MainActivity() { 
+    }
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+//        Platform.Init(this, savedInstanceState);
+
+//        var s = SystemUiFlags.LayoutFullscreen | SystemUiFlags.LayoutStable;
+//#pragma warning disable CS0618 // Type or member is obsolete
+//        FindViewById(Android.Resource.Id.Content).SystemUiVisibility =
+//        (StatusBarVisibility)s;
+//#pragma warning restore CS0618 // Type or member is obsolete
+//        Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+//        WindowInsetsControllerCompat windowInsetsController = new
+//        WindowInsetsControllerCompat(Window, Window.DecorView);
+//        windowInsetsController.AppearanceLightStatusBars = false;
+
+//        MainActivityInstance = this;
+    }
+
+    public static void SetStatusBarLight(bool isLight)
+    {
+        if (MainActivityInstance != null)
+        {
+            WindowInsetsControllerCompat windowInsetsController =
+            new WindowInsetsControllerCompat(MainActivityInstance.Window, MainActivityInstance.Window.DecorView);
+            windowInsetsController.AppearanceLightStatusBars = isLight;
+        }
+    }
 }
